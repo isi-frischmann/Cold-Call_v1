a7461ec62191de604ec70adce45494364e6c76f5
isi-frischmann <isabell.frischmann@gmail.com>
2018-09-05 20:36:55 -0700

    second update

--- Cold Call/ViewController.swift
+++ Cold Call/ViewController.swift
@@ -12,15 +12,30 @@ class ViewController: UIViewController {
     
 //    if button is clicked the names should be shown in this field
     @IBOutlet weak var showName: UITextField!
+    @IBOutlet weak var showNumber: UITextField!
     
-//    create an array with names
+    //    create an array with names
     let names: [String] = ["Isabell", "Manuel", "Franz", "Lucas", "Daisy", "Steffi", "Gitti"]
-
+    
 //    when button is clicked show the names with the index who is choosed from the random index
     @IBAction func coldCallPressed(_ sender: UIButton) {
-//        print("Button is clicked")
-        let randomIndex = Int(arc4random_uniform(UInt32(7)))
-        showName.text = names[randomIndex]
+//        create random number for the indexes of the names array and then show the name
+        let randomIndexNames = Int(arc4random_uniform(7)) //creates random number from 1 - 6
+        let randomNumber = Int(arc4random_uniform(5) + 1) //creates random number from 1 - 5
+        
+        showName.text = names[randomIndexNames]
+        
+        showNumber.text = "\(randomNumber)"
+        
+        if randomNumber == 1 || randomNumber == 2 {
+            showNumber.textColor = .red
+        }
+        if randomNumber == 3 || randomNumber == 4 {
+            showNumber.textColor = .orange
+        }
+        if randomNumber == 5 {
+            showNumber.textColor = .green
+        }
     }
     
 
